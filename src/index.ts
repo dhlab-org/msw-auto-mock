fa89281b3a08268cc17378982ab00e708925ae7a
milk717 <robolindasoo@gmail.com>
2025-07-08 14:43:21 +0900

    🔥 불필요한 export를 제거한다

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 // 환경 상관없이 사용 가능한 기능들
 export { selectResponseByScenario } from './scenario-selector';
-export type { TOptions, TStreamingEvent, TScenarioConfig } from './types';
+export type { TOptions, TStreamingEvent } from './types';
