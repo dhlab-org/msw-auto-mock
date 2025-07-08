c29a4f45124fc573415305ef6a0067ab0d424f54
milk717 <robolindasoo@gmail.com>
2025-07-08 14:42:46 +0900

    🔥 불필요한 export를 제거한다

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 // 환경 상관없이 사용 가능한 기능들
 export { selectResponseByScenario } from './scenario-selector';
-export { transformJSONSchemaToFakerCode, MAX_STRING_LENGTH } from './faker';
 export type { TOptions, TStreamingEvent, TScenarioConfig } from './types';
